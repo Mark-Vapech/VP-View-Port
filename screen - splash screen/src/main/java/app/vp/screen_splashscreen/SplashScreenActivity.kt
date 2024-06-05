eead932cf4635e932cd10ef07c11e81e9cf791b8
EFINANCETHAI\ratthakan.v <ratthakan@efinancethai.com>
2024-06-05 17:56:08 +0700

    first project build

---
+++ screen - splash screen/src/main/java/app/vp/screen_splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package app.vp.screen_splashscreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreenActivity(
+    modifier: Modifier = Modifier
+) {
+    Text(text = "SplashScreen")
+}
