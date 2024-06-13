a4896995a64ce000dda2c09acf71b7092890d753
EFINANCETHAI\ratthakan.v <ratthakan@efinancethai.com>
2024-06-13 15:53:17 +0700

    build project

--- app/src/main/java/app/vp/vp_viewport/MainActivity.kt
+++ app/src/main/java/app/vp/vp_viewport/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        println("VPViewPortTheme")
         setContent {
             VPViewPortTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
